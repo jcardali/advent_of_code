4a53465d42f810161f97f9e5b8580c529873404d
Joe Cardali <jcardali@better.com>
2021-12-02 12:32:58 -0600

    Setup + 2021-1

---
+++ 2021/1/2.py
@@ -0,0 +1,24 @@
+f = open("input.txt")
+
+num_arr = []
+sums = {}
+
+for line in f:
+    cur_num = int(line)
+    num_arr.append(cur_num)
+
+for idx, num in enumerate(num_arr):
+    try:
+        sums[idx] = num_arr[idx] + num_arr[idx+1] + num_arr[idx+2]
+    except:
+        pass
+
+prev_sum = 0
+increases = 0
+
+for sum in sums.values():
+    if sum > prev_sum:
+        increases += 1
+    prev_sum = sum
+
+print(increases-1)
