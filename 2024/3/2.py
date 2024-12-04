5156024d09327d53c6af01dff87b47d41d8f4d0b
Joe Cardali <joe.cardali@datadoghq.com>
2024-12-04 12:52:22 -0500

    Remove unnecessary comparisons

--- 2024/3/2.py
+++ 2024/3/2.py
@@ -13,11 +13,11 @@ sum = 0
 is_enabled = True
 
 for match in matches:
-    if match[0] != "" and is_enabled:
+    if match[0] and is_enabled:
         sum += do_mul(match[0])
-    elif match[1] != "":
+    elif match[1]:
         is_enabled = True
-    elif match[2] != "":
+    elif match[2]:
         is_enabled = False
 
 print(sum)
