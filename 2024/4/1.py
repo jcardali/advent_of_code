212838c83b19a8c73c8bef2d9c4ec01d46dd2ca1
Joe Cardali <joe.cardali@datadoghq.com>
2024-12-05 19:31:55 -0500

    Update 4.1

--- 2024/4/1.py
+++ 2024/4/1.py
@@ -1,5 +1,3 @@
-search_term = "XMAS"
-
 global grid
 grid = []
 
@@ -8,24 +6,27 @@ with open("input.txt") as f:
         row = []
         for char in line.strip():
             row.append(char)
-        print(row)
         grid.append(row)
 
-# shifts = [
-#     (0, 1),
-#     (0, -1),
-# ]
-
-"""
-
-"""
 count = 0
 for y, row in enumerate(grid):
     for x, col in enumerate(row):
+        # shifts = [
+        #     [(0, 0), (0, 1), (0, 2), (0, 3)],
+        #     [(0, 0), (1, 0), (2, 0), (3, 0)],
+        #     [(0, 0), (1, 1), (2, 2), (3, 3)],
+        #     [(0, 0), (1, -1), (2, -2), (3, -3)],
+        # ]
+        # for shift in shifts:
+        #     try:
+        #         word = grid[y + shift[0][0]][x + shift[0][0]] + grid[y + shift[1][0]][x + shift[1][1]] + grid[y + shift[2][0]][x + shift[2][1]] + grid[y + shift[3][0]][x + shift[3][1]]
+        #         if word == "XMAS" or word == "SAMX":
+        #             count += 1
+        #     except IndexError:
+        #         pass
         try:
             word = grid[y][x] + grid[y][x + 1] + grid[y][x + 2] + grid[y][x + 3]
             if word == "XMAS" or word == "SAMX":
-                # print("horizontal", (y, x), (y, x + 1), (y, x + 2), (y, x + 3))
                 count += 1
         except IndexError:
             pass
@@ -33,7 +34,6 @@ for y, row in enumerate(grid):
         try:
             word = grid[y][x] + grid[y + 1][x] + grid[y + 2][x] + grid[y + 3][x]
             if word == "XMAS" or word == "SAMX":
-                # print("vertical", (y, x), (y + 1, x), (y + 2, x), (y + 3, x))
                 count += 1
         except IndexError:
             pass
@@ -41,24 +41,19 @@ for y, row in enumerate(grid):
         try:
             word = grid[y][x] + grid[y + 1][x + 1] + grid[y + 2][x + 2] + grid[y + 3][x + 3]
             if word == "XMAS" or word == "SAMX":
-                # print("diagonal right", (y, x), (y + 1, x + 1), (y + 2, x + 2), (y + 3, x + 3))
                 count += 1
         except IndexError:
             pass
 
         try:
             word = grid[y][x] + grid[y + 1][x - 1] + grid[y + 2][x - 2] + grid[y + 3][x - 3]
+            # Need to make sure our index doesn't go negative
             if (word == "XMAS" or word == "SAMX") and x >= 3:
-                # print("diagonal left", (y, x), (y + 1, x - 1), (y + 2, x - 2), (y + 3, x - 3))
                 count += 1
         except IndexError:
             pass
 
 print(count)
-
-# row_count = len(grid) - 1
-# col_count = len(grid[0]) - 1
-# print(row_count, col_count)
 
 # def search(y, x, search_term):
 #     if y < 0 or x < 0:
@@ -68,11 +63,8 @@ print(count)
 #         val = grid[y][x]
 #     except IndexError:
 #         return False
-#
-#     # print(y, x, search_term)
-#
+
 #     if val == search_term:
-#         # print("here")
 #         return True
 #     if val == search_term[0]:
 #         return (
